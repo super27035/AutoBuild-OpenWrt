6bf4cc4e256c77ac2dea15c0f0f684561afc7173
super27035 <43180500+super27035@users.noreply.github.com>
2021-03-03 23:24:14 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -9,3 +9,4 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/SUPERrouter/g' openwrt/package/base-files/files/bin/config_generate
