bd4fc986572c2e4c832b0ecd0061fd4f99287693
super27035 <43180500+super27035@users.noreply.github.com>
2021-03-03 23:30:39 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,6 +6,7 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 # 替换文件中的所有匹配项
 # sed -i 's/原字符串/替换字符串/g' filename
+# 在openwrt/package/base-files/files/bin/config_generate下可以修改IPV6设置
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
