643c12cd249c15a2f903d9665f96c490759e94bb
super27035 <43180500+super27035@users.noreply.github.com>
2021-03-03 23:25:08 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -9,4 +9,5 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+#2. Modify default hosename
 sed -i 's/OpenWrt/SUPERrouter/g' openwrt/package/base-files/files/bin/config_generate
