24b265d384fcbd3986e86fe21327b3f302876473
super27035 <43180500+super27035@users.noreply.github.com>
2021-03-04 18:54:23 +0800

    add new password v2

--- super_v1.0.sh
+++ super_v1.0.sh
@@ -16,4 +16,4 @@ sed -i 's/OpenWrt/SUPERouter/g' openwrt/package/base-files/files/bin/config_gene
 #3. Change the login password
 # $1$S2TRFyMU$E8fE0RRKR0jNadn3YLrSQ0:18690:0:99999:7::: #superlyn
 # $1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::     #password
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/$1$S2TRFyMU$E8fE0RRKR0jNadn3YLrSQ0:18690:0:99999:7:::/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/$1$S2TRFyMU$E8fE0RRKR0jNadn3YLrSQ0:18690:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
