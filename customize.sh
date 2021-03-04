bb38d793b1253a895a54c2b201cf39328aa9e49e
super27035 <43180500+super27035@users.noreply.github.com>
2021-03-04 09:46:47 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -13,7 +13,7 @@ sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config
 #2. Modify default hosename
 sed -i 's/OpenWrt/SUPERrouter/g' openwrt/package/base-files/files/bin/config_generate
 #3. Modify default login password,needs to disable this funtion @ package/lean/default-settings/files/zzz-default-settings
-#sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
+#sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow  #original settings from the shadow file
 #root:$1$S2TRFyMU$E8fE0RRKR0jNadn3YLrSQ0:18690:0:99999:7::: #superlyn
 #root:$1$UF4foXSz$b8bNHSHxA3Ql99IPeTFNi1:18690:0:99999:7::: #super
 sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$S2TRFyMU$E8fE0RRKR0jNadn3YLrSQ0:18690:0:99999:7:::/g' openwrt/package/base-files/files/etc/shadow
