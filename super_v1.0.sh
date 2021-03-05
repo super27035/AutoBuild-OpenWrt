37a15be2e4589b2a6e9710bd547c95a0ca4b0f30
super27035 <43180500+super27035@users.noreply.github.com>
2021-03-06 06:45:53 +0800

    change the login ip

--- super_v1.0.sh
+++ super_v1.0.sh
@@ -8,7 +8,7 @@
 # sed -i 's/原字符串/替换字符串/g' filename
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.123.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. Modify default hosename
 sed -i 's/OpenWrt/SUPERouter/g' openwrt/package/base-files/files/bin/config_generate
