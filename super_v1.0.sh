960d897d46e34dc82a4c414cdf54352857ee4d38
super27035 <43180500+super27035@users.noreply.github.com>
2021-03-05 22:42:01 +0800

    Update super_v1.0.sh

--- super_v1.0.sh
+++ super_v1.0.sh
@@ -11,7 +11,7 @@
 sed -i 's/192.168.1.1/192.168.123.1/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. Modify default hosename
-sed -i 's/OpenWrt/SUPERouter™/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/SUPERouter/g' openwrt/package/base-files/files/bin/config_generate
 
 #3. Change the login password
 # $1$S2TRFyMU$E8fE0RRKR0jNadn3YLrSQ0:18690:0:99999:7::: #superlyn
