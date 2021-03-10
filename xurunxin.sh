b03b1c51b8a8c77c353a85ddfa8a5039a9ba8f16
super27035 <43180500+super27035@users.noreply.github.com>
2021-03-10 09:31:01 +0800

    Create xurunxin.sh

---
+++ xurunxin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: eSirPlayground
+# Youtube Channel: https://goo.gl/fvkdwm 
+#=================================================
+#1. Modify default IP
+cd openwrt
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase="/luci-static/argon"' package/lean/default-settings/files/zzz-default-settings
