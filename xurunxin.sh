93b0a3b169ad7f53ecf79228d42c746843444567
super27035 <43180500+super27035@users.noreply.github.com>
2021-03-10 10:17:03 +0800

    Update xurunxin.sh

--- xurunxin.sh
+++ xurunxin.sh
@@ -6,6 +6,6 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-cd openwrt
-sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
-sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase="/luci-static/argon"' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase="/luci-static/argon"' openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i 's/zh_cn/auto/g' openwrt/package/lean/default-settings/files/zzz-default-settings
