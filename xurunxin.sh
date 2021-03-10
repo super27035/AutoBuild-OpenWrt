46ff65df4bd799b2e21fb21257ad920b121fc3ad
super27035 <43180500+super27035@users.noreply.github.com>
2021-03-10 13:02:27 +0800

    Update xurunxin.sh

--- xurunxin.sh
+++ xurunxin.sh
@@ -8,4 +8,4 @@
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase="/luci-static/argon"' openwrt/package/lean/default-settings/files/zzz-default-settings
-sed -i 's/zh_cn/auto/g' openwrt/package/lean/default-settings/files/zzz-default-settings
+#sed -i 's/zh_cn/auto/g' openwrt/package/lean/default-settings/files/zzz-default-settings
