8d0d7c3f919e54c71a5b3732bd41e6d706a36d08
super27035 <43180500+super27035@users.noreply.github.com>
2021-03-10 09:44:06 +0800

    Update external_feeds.sh

--- external_feeds.sh
+++ external_feeds.sh
@@ -1,5 +1,12 @@
 
-cd openwrt/package/lean/
-git clone https://github.com/xiaorouji/openwrt-passwall.git
+#cd openwrt/package/lean/
+#git clone https://github.com/xiaorouji/openwrt-passwall.git
 # git clone https://github.com/kongfl888/luci-app-r2sflasher.git
 # git clone https://github.com/pymumu/smartdns.git
+
+
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/penwrt-passwall
+git clone https://github.com/super27035/OpenClash.git package/OpenClash
+git clone -b 18.06 https://github.com/super27035/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/super27035/lua-maxminddb.git package/lua-maxminddb
+git clone https://github.com/super27035/luci-app-vssr.git package/luci-app-vssr
